31a8a81f82077ed5b4ae5712575b9222c5b270eb
andrewkozinski <andrewkozinski76@gmail.com>
2024-10-08 21:52:42 -0400

    Updated gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,12 @@
 const Gallery = ({images}) => {
     return (
       <div>
-        
-        
-
+        {images && images.length > 0 ? 
+        (
+            <div>{images.map((image, index) => <img key={index} src={image} alt={`Screenshot ${index}`} />)}</div>
+        ) 
+        : 
+        ( <div> <h3>You haven't made a screenshot yet!</h3> </div> )}
       </div>
     )
   }
