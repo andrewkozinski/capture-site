68089bf56e14e13460e59b0e4547d06347c5589f
andrewkozinski <andrewkozinski76@gmail.com>
2025-03-18 19:44:20 -0400

    Added some in line stylings to images in Gallery.jsx for the previous images to display better

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,24 @@
 const Gallery = ({images}) => {
+
+  const containerStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '10px',
+    maxWidth: '100%',
+    overflow: 'hidden',
+  };
+
+  const imageStyle = {
+    maxWidth: '100%',
+    height: 'auto',
+    objectFit: 'cover',
+  };
+
     return (
       <div>
         {images && images.length > 0 ? 
         (
-            <div>{images.map((image, index) => <img key={index} src={image} alt={`Screenshot ${index}`} />)}</div>
+            <div style={containerStyle}>{images.map((image, index) => <img key={index} src={image} alt={`Screenshot ${index}`} style={imageStyle}/>)}</div>
         ) 
         : 
         ( <div> <h3>You haven't made a screenshot yet!</h3> </div> )}
